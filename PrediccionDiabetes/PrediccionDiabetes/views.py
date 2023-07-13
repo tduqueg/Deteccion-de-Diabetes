2eb41b4867ce511f68442bd76f8b2583569dcb14
tduqueg <tduqueg@eafit.edu.co>
2023-07-13 15:06:07 -0500

    Cambios en predicción

--- PrediccionDiabetes/PrediccionDiabetes/views.py
+++ PrediccionDiabetes/PrediccionDiabetes/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+
+
+
 def home(request):
     return render(request,'home.html')
 def predict(request):
     return render(request,'predict.html')
+def result(request):
+    data = pd.read_csv('Detección de Diabetes/PrediccionDiabetes/PredicciónDiabetes/diabetes.csv')
+
+
+    X = data.drop("Outcome", axis=1)
+    Y = data["Outcome"]
+
+    X_train,X_test,Y_train,Y_test = train_test_split(X,Y, test_size= 0.2 )
+
+    model = LogisticRegression()
+    model.fit(X_train,Y_train)
+
+    val1 = float(request.GET['n1'])
+    val2 = float(request.GET['n2'])
+    val3 = float(request.GET['n3'])
+    val4 = float(request.GET['n4'])
+    val5 = float(request.GET['n5'])
+    val6 = float(request.GET['n6'])
+    val7 = float(request.GET['n7'])
+    val8 = float(request.GET['n8'])
+
+    pred = model.predict([[val1,val2,val3,val4,val5,val6,val7,val8]])
+
+    result1 = ""
+
+    if pred == [1]:
+        result1 = "Lo más probable es que tengas diabetes, consulta lo antes posible con un médico"
+    else:
+        result1 = "Felicidades! No tienes diabetes, pero no olvides cuidarte"
+
+    return render(request,'predict.html',{"result2":result1})
